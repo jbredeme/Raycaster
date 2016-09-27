4d5eee9d69c1a79ba2ddeb83b7c6a2c68f382f09
jbredeme <jpb64@nau.edu>
2016-09-27 00:14:49 -0700

    One more before bedtime!

--- main.c
+++ main.c
@@ -11,9 +11,37 @@
 #include <ctype.h>
 #include "json.h"
 
-int main(int argc, char *argv[]){
-	FILE *fpointer;
+/**
+ * json_obj
+ *
+ *
+ */
+typedef struct json_obj {
+	char *type;
+	double radius;
+	double width;
+	double height;
+	double *color_vector;
+	double *position_vector;
+	double *normal_vector;
 	
+} json_obj;
+
+
+
+int main(int argc, char *argv[]){
+	
+	FILE *fpointer;
+	json_obj *scene;
+	
+	scene = (json_obj *)malloc(128 * sizeof(json_obj));
+	if(scene == NULL) {
+		fprintf(stderr, "Error, memory allocation failed.\n");
+		// Close file stream flush all buffers
+		fclose(fpointer);
+		exit(-1);		
+	}
+
 	fpointer = fopen("test01_good.txt", "r");
 	
 	if(fpointer == NULL) {
